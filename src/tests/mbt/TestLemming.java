21e9d18eba0772d821846ee057c5dc6d35bffa91
Felix <Felix@pc-felix>
2016-05-04 22:45:17 +0200

    TestLemming

--- src/tests/mbt/TestLemming.java
+++ src/tests/mbt/TestLemming.java
@@ -36,6 +36,19 @@ public class TestLemming {
 		lemming.init(gameEng);
 	}
 	
+	
+	/**
+	 * Objectif de Test: setHeight(int height) reussi
+	 * 
+	 * Cas de Test: gameEng.setHeight(int height)
+	 * 		 0 < height < getGameEng().getLevel().getHeight()
+	 * 
+	 * Condition initiale:
+	 * Aucune
+	 * 
+	 * Operation:
+	 *  setHeight(5)
+	 */
 	@Test
 	public void testSetHeight(){
 		int height=5;
@@ -48,8 +61,19 @@ public class TestLemming {
 				height<gameEng.getLevel().getHeight());
 	}
 	
+	
+	
 	/**
-	 * heiht<=0
+	 * Objectif de Test: setHeight(int height) failed heiht<=0
+	 * 
+	 * Cas de Test: gameEng.setHeight(int height)
+	 * 		 height<=0
+	 * 
+	 * Condition initiale:
+	 * Aucune
+	 * 
+	 * Operation:
+	 *  setHeight(0)
 	 */
 	@Test
 	public void testSetHeight2(){
@@ -63,8 +87,19 @@ public class TestLemming {
 				height<gameEng.getLevel().getHeight());
 	}
 	
+	
+	
 	/**
-	 * heiht<=getLevel().getHeight()
+	 * Objectif de Test: setHeight(int height) failed height<=getLevel().getHeight()
+	 * 
+	 * Cas de Test: gameEng.setHeight(int height)
+	 * 		height>= getLevel().getHeight()
+	 * 
+	 * Condition initiale:
+	 * Aucune
+	 * 
+	 * Operation:
+	 *  setHeight(gameEng.getLevel().getHeight())
 	 */
 	@Test
 	public void testSetHeight3(){
@@ -78,26 +113,49 @@ public class TestLemming {
 				height<gameEng.getLevel().getHeight());
 	}
 	
-	@Test
-	public void testSetWidth(){
-		int height=5;
-		
-		assertTrue("height a tester '"+height+"' height>0 ?",
-				height>0);
-		
-		assertTrue("height a tester '"+height+"' getLevel().getHeight() = "+
-		gameEng.getLevel().getHeight()+" height<getLevel().getHeight() ?",
-				height<gameEng.getLevel().getHeight());
-	}
 	
 	/**
-	 * width<=0
+	 * Objectif de Test: setWidth(int width) reussi
+	 * 
+	 * Cas de Test: gameEng.setWidth(int width)
+	 * 		 0 < width < getGameEng().getLevel().getWidth()
+	 * 
+	 * Condition initiale:
+	 * Aucune
+	 * 
+	 * Operation:
+	 *  setWidth(5)
+	 */
+	@Test
+	public void testSetWidth(){
+		int width=5;
+		
+		assertTrue("width a tester '"+width+"' width>0 ?",
+				width>0);
+		
+		assertTrue("width a tester '"+width+"' getLevel().getWidth() = "+
+		gameEng.getLevel().getWidth()+" width<getLevel().getWidth() ?",
+		width<gameEng.getLevel().getWidth());
+	}
+	
+	
+	/**
+	 * Objectif de Test: setWidth(int width) failed width<= 0 
+	 * 
+	 * Cas de Test: gameEng.setWidth(int width)
+	 * 		 width<= 0 
+	 * 
+	 * Condition initiale:
+	 * Aucune
+	 * 
+	 * Operation:
+	 *  setWidth(0)
 	 */
 	@Test
 	public void testSetWidth2(){
 		int width=0;
 		
-		assertTrue("width a tester '"+width+"' height>0 ?",
+		assertTrue("width a tester '"+width+"' width>0 ?",
 				width>0);
 		
 		assertTrue("width a tester '"+width+"' getLevel().getWidth() = "+
@@ -105,8 +163,18 @@ public class TestLemming {
 		width<gameEng.getLevel().getWidth());
 	}
 	
+	
 	/**
-	 * width<=getLevel().getWidth()
+	 * Objectif de Test: setWidth(int width) failed width >= getGameEng().getLevel().getWidth()
+	 * 
+	 * Cas de Test: gameEng.setWidth(int width)
+	 * 		 width >= getGameEng().getLevel().getWidth()
+	 * 
+	 * Condition initiale:
+	 * Aucune
+	 * 
+	 * Operation:
+	 *  setWidth(getGameEng().getLevel().getWidth())
 	 */
 	@Test
 	public void testSetWidth3(){
